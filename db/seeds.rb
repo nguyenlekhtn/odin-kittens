9a4fd96c485355e459c3b8d9d183fa4912e34a0d
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-04-24 15:44:34 +0700

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Kitten.delete_all
+
+Kitten.create(name: "Mew", age: 1000, cuteness: 10, softness: 10)
+Kitten.create(name: "Purrloin", age: 8, cuteness: 5, softness: 8)
+Kitten.create(name: "Meowt", age: 10, cuteness: 2, softness: 3)
+Kitten.create(name: "Sprigatito", age: 3, cuteness: 8, softness: 8)
+Kitten.create(name: "Litten", age: 4, cuteness: 9, softness: 7)
