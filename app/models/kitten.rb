ee1d632ab041e284365f11d86d4bf820c55e1f12
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-04-24 15:45:56 +0700

    add length constaint for name property

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,5 +1,6 @@
 class Kitten < ApplicationRecord
   validates :name, :age, :cuteness, :softness, presence: true
+  validates :name, length: { minimum: 1, maximum: 20 }
   validates :softness, :cuteness, numericality: { in: 0..10 }
   validates :age, numericality: { greater_than_or_equal_to: 0 }
 end
